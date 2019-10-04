e9f49a19e17c34cc71b06f7f8e3cd5e3209a8542
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-10-04 20:56:17 +0100

    Creating a form to log a user in

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from accounts.forms import LoginForm
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+            
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('home'))
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = LoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
 
 def logout(request):
     auth.logout(request)
