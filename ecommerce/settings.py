98eacaa4652d158d5c0ca320731f4ca9d97a291c
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-10-04 19:10:36 +0100

    Created a home app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Added apps
     'accounts',
+    'home',
 ]
 
 MIDDLEWARE = [
