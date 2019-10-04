54570ca717c7958958a5903ac9b070b1cfd39647
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-10-04 19:50:06 +0100

    Created a logout with message

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 def login(request):
     return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You have logged out successfully')
+    return redirect(reverse('home'))
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from accounts.views import login
+from accounts.views import login, logout
 from home.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
