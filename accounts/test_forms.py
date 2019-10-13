fb2e2b650d7bc10b525633ed92be3f1c6983b152
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-10-13 12:24:04 +0100

    Added testing for a superuser

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -32,3 +32,18 @@ class TestUserForms(TestCase):
             User.objects.create_user(email='')
         with self.assertRaises(ValueError):
             User.objects.create_user(email='', password='foo', username='')
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email='super@user.com', password='foo', username='normaljoe101')
+        self.assertEqual(admin_user.email, 'super@user.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            self.assertIsNotNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='super@user.com', password='foo', username='normaljoe101', is_superuser=False)
