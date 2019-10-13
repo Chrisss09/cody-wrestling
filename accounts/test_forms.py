c7b233fb407792b8b6097712ff7a3df35777a109
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-10-13 11:52:08 +0100

    Added testing to my accounts form

---
+++ accounts/test_forms.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from .forms import UserRegistrationModel, LoginForm, User
+from django.urls import reverse, resolve
+from django.contrib.auth import get_user_model
+
+class TestUserForms(TestCase):
+
+    def test_if_progress_made_if_not_inserted_required_fields(self):
+        form = LoginForm({'name': 'Create Tests'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [u'This field is required.'])
+
+    def test_login_form_no_data(self):
+        form = LoginForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 2)
+
+    def test_custom_model(self):
+        User = get_user_model()
+        user = User.objects.create_user(email='normal@user.com', password='foo', username='normaljoe101')
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        try:
+            self.assertIsNotNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password='foo', username='')
