2cd2e475b098b00c86bcff1218be3c1badefb719
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-10-22 21:55:21 +0100

    Created and migrated a product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    category = models.CharField(max_length=10)
+    release_date = models.DateField()
+    summary = models.CharField(max_length=150)
+    description = models.TextField()
+    user_likes = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images')
+
+    def __unicode__(self):
+        return self.title
