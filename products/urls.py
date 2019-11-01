56c363fe6da7ef1f1d84a5b93b5e3c02db04cf9e
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-11-01 14:51:03 +0000

    Added a PK with links

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from products.views import category
+from products.views import category, view_products, view_more_info
 
 urlpatterns = [
     path('company', category, name='products'),
+    path('company/dvds/', view_products, name='view_products'),
+    path('<dvd_id>/', view_more_info, name='view_more_info'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def category(request):
-    product = Product.objects.all()
-    return render(request, 'company.html', {'product': product})
+    categories = Product.objects.all()
+    return render(request, 'company.html', {'categories': categories})
     
+def view_products(request):
+    company_range = Product.objects.all()
+    return render(request, 'products.html', {'company_range': company_range})
+
+def view_more_info(request, dvd_id):
+    dvd_detail = get_object_or_404(Product, pk=dvd_id)
+    return render(request, 'dvdinfo.html', {'dvd': dvd_detail})
